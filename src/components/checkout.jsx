62d0f3cddd18d0f4cb991d73bf97cd52f8c2f18d
Raje <rajeshwar.work09@gmail.com>
2025-03-04 17:43:15 +0530

    feat: Add order submission functionality

--- src/components/checkout.jsx
+++ src/components/checkout.jsx
@@ -18,15 +18,35 @@ const Checkout = () => {
     userProgressCtx.hideCheckout();
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Submit order
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries());
+
+    fetch("http://localhost:3000/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        order: {
+          customer: customerData,
+          items: cartCtx.items,
+        },
+      }),
+    });
+  };
+
   return (
     <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
 
-        <Input label="Full Name" type="text" id="full-name" />
+        <Input label="Full Name" type="text" id="name" />
         <Input label="Email" type="email" id="email" />
-        <Input label="Address" type="text" id="address" />
+        <Input label="Street" type="text" id="street" />
         <div className="control-row">
           <Input label="City" type="text" id="city" />
           <Input label="Postal Code" type="text" id="postal-code" />
